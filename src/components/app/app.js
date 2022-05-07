e368ca253b3a60ad7ccb965f7e4df5ba107b69f4
Sergei Bespalov <sergei.bespalov@sap.com>
2022-05-07 16:14:02 +0300

    route to login

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import './app.css';
 import LoginPage from "../pages/login-page";
 import SwapiService from '../../services/swapi-service'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 export default class App extends Component {
 
@@ -34,6 +35,25 @@ export default class App extends Component {
                 <LoginPage isLoggedIn={this.state.isLoggedIn}
                            onLogin={this.onLogin}/>
             </div>
+        );
+    }
+
+    render() {
+        const {isLoggedIn} = this.state;
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route
+                            path="/login"
+                            render={() => (
+                                <LoginPage isLoggedIn={this.state.isLoggedIn}
+                                           onLogin={this.onLogin}/>
+                            )}/>
+                    </Switch>
+
+                </div>
+            </Router>
         );
     }
 }
