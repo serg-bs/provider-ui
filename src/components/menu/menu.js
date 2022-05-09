0ad4eb51bcae3fd21ded75634a82c538c72b2a2e
(Dmitryi) <geradinplay@gmail.com>
2022-05-09 16:47:21 +0200

    Изменена структура меню, удалены ненужны css классы.

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,39 +3,21 @@ import './menu.css';
 import {Link} from "react-router-dom";
 
 export default class Menu extends Component {
-    state = {
-        done: false
-    };
-    MainClick = () => {
-        this.setState(({done}) => {
-            return {
-                done: !done
-            }
-        });
-    };
-
     render() {
-        const {done} = this.state;
-
-        let class1 = 'style ';
-        if (done) {
-            class1 += 'Status';
-        }
-        console.log({class1})
-
+const class1="btn btn-secondary"
         return (
             <ul className="list">
                 <il>
-                    <Link to="/" ><button className={class1}>Главная</button></Link>
+                    <Link to="/" ><button type="button" className={class1}>Главная</button></Link>
                 </il>
-                <il className="block-2">
-                    <button className={class1}> Тарифы</button>
+                <il>
+                    <button type="button" className={class1}> Тарифы</button>
                 </il>
-                <il className="block-3">
-                    <button className={class1}> Платежи</button>
+                <il>
+                    <button type="button" className={class1}> Платежи</button>
                 </il>
-                <il className="block-4">
-                    <button className={class1}> Инфо</button>
+                <il>
+                    <button type="button" className={class1}> Инфо</button>
                 </il>
             </ul>
         )
