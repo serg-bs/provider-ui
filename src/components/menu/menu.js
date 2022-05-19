0dc4c1db39bd041c2abde12f093b8dd76ac6a418
(Dmitryi) <geradinplay@gmail.com>
2022-05-19 12:29:21 +0200

    Начал разработку вкладки информации

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -5,25 +5,16 @@ import History from "../pay-history/history";
 
 export default class Menu extends Component {
     render() {
-const class1="btn btn-secondary"
+        const class1 = "btn btn-secondary"
         return (
-            <ul className="list">
-                <il>
-                    <Link to="/" ><button type="button" className={class1}>Главная</button></Link>
-                </il>
-                <il>
-                    <Link to="/tariff" ><button type="button" className={class1}> Тарифы</button></Link>
-                </il>
-                <il>
-                    <Link to="/account" ><button type="button" className={class1}> Оплата</button></Link>
-                </il>
-                <il>
-                    <Link to="/payments" ><button type="button" className={class1}> История Платежей</button></Link>
-                </il>
-                <il>
-                    <Link to="/client" ><button type="button" className={class1}> Инфо</button></Link>
-                </il>
-            </ul>
+            <div className="list btn-group list" role="group" aria-label="Basic example">
+                <Link className={class1} to="/">Главная</Link>
+                <Link className={class1} to="/tariff">Тарифы</Link>
+                <Link className={class1} to="/account">Оплата</Link>
+                <Link className={class1} to="/payments">Платежи</Link>
+                <Link className={class1} to="/client">Инфо</Link>
+
+            </div>
         )
     };
 }
--- src/services/dummy-swapi-service.js
+++ src/services/dummy-swapi-service.js
@@ -2,57 +2,58 @@ import AccountPage from "../components/account-page";
 
 export default class DummySwapiService {
 
-  _client = {
-    id: 2,
-    name: "Patrik",
-    surename: "Collman",
-    middlename: "Michailovich",
-    address: "Baturina",
-    login: "serg",
-    phone: "+7910999999",
-    email: "patric@yandex.ru"
-  }
-
-  _tariffs = [
-      { name: "My first tariff", speed: 100, price: 1000},
-      { name: "My second tariff", speed: 2000, price: 2000},
-      { name: "My third tariff", speed: 9000, price: 3000}
-    ];
-
-  getTariffs = async () => {
-    return this._tariffs;
-  };
-  _payments = [
-    {
-      amount: 100000000,
-      payDateTime: "2022-05-11T09:21:03.368Z",
+    _client = {
+        id: 2,
+        name: "Patrik",
+        surename: "Collman",
+        middlename: "Michailovich",
+        address: "Baturina",
+        login: "serg",
+        phone: "+7910999999",
+        email: "patric@yandex.ru"
     }
+
+    _tariffs = [
+        {name: "My first tariff", speed: 100, price: 1000},
+        {name: "My second tariff", speed: 2000, price: 2000},
+        {name: "My third tariff", speed: 9000, price: 3000}
     ];
-_account =
-    {
-  address: "Vladimirskay oblaststreet Baturina 37",
-  balance: 12121,
-  status: true
-}
-  getAccount = async (jwt) => {
-  return this._account;
-  }
 
-  updateAccount = async (payment, jwt) => {
-      // alert(payment.amount)
-  }
+    getTariffs = async () => {
+        return this._tariffs;
+    };
+    _payments = [
+        {
+            amount: 100000000,
+            payDateTime: "2022-05-11T09:21:03.368Z",
+        }
+    ];
+    _account =
+        {
+            address: "Vladimirskay oblaststreet Baturina 37",
+            balance: 12121,
+            status: true,
+            tariffname: "Забугорище!!!"
+        }
+    getAccount = async (jwt) => {
+        return this._account;
+    }
 
-  addPayment = async (payment, jwt) => {
-    console.log(payment)
-    alert(payment)
-  };
+    updateAccount = async (payment, jwt) => {
+        // alert(payment.amount)
+    }
 
-  getPayments = async (jwt) => {
-    return this._payments;
-  };
+    addPayment = async (payment, jwt) => {
+        console.log(payment)
+        alert(payment)
+    };
 
-  async getClient(id, jwt) {
-    return this._client;
-  };
+    getPayments = async (jwt) => {
+        return this._payments;
+    };
+
+    async getClient(id, jwt) {
+        return this._client;
+    };
 
 }
