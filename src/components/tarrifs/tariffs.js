966c731164a57b5370b9f4b621eb44fa2b72ff59
(Dmitryi) <geradinplay@gmail.com>
2022-05-19 22:17:55 +0200

    Начал разработку вкладки информации

--- src/components/tarrifs/tariffs.js
+++ src/components/tarrifs/tariffs.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "./list.css"
 import Tariffsitems from "./tariffsitems";
 const Tariffs = (props) => {
+    const [tariffname,settariffname] = useState('');
+    const [tariffspeed,settariffspeed] = useState('');
+    const [tariffprice,settariffprice] = useState('');
     const {tariffData, updateTariff} = props;
 
     const elements = tariffData.map((item) => {
@@ -10,16 +13,17 @@ const Tariffs = (props) => {
         )
     })
     console.log(elements)
+    const class3 = ('form-control')
     return (
         <div className="person-details card top">
         <table className="table position">
-            <thead className="list-head">
+            <thead className="list-head ">
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">name</th>
-                <th scope="col">Speed</th>
-                <th scope="col">Price</th>
-                <th scope="coll">connect</th>
+                <th scope="col"><button className="btn btn-success form-control">ADD</button></th>
+                <th scope="col"><input className={class3} placeholder="Название" value={tariffname} onChange={o => settariffname(o.target.value)}></input></th>
+                <th scope="col"><input className={class3} placeholder="Скорость" value={tariffspeed} onChange={o => settariffspeed(o.target.value)}></input></th>
+                <th scope="col"><input className={class3} placeholder="Цена" value={tariffprice} onChange={o => settariffprice(o.target.value)}></input></th>
+                <th>-------------</th>
             </tr>
             </thead>
             <tbody>{elements}</tbody>
